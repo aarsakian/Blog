3e8f97fa937e31cf7d6f7548faa9142f55992266
Armen Arsakian <aarsakian@gmail.com>
2013-10-05 21:49:03 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-DEBUG=True
-SECRET_KEY='dev_425key_harfgsd324m'
-CSRF_ENABLED=True
-CSRF_SESSION_KEY='dev_keyasdarv.a/+'
-#user:password@host/dbname
-SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms://armen:sobelius@google.com/test'
-
-
