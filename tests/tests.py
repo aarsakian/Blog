b16b97b6905b78d687f9a2fb9af34ac4b5edbd32
Armen Arsakian <arsakian@ee.duth.gr>
2016-11-01 21:25:37 +0200

    still working

--- tests/tests.py
+++ tests/tests.py
@@ -5,6 +5,7 @@ from blog import app
 from blog.views import CODEVERSION, fetch_everything_from_db, calculate_work_date_stats
 from google.appengine.ext import testbed
 from google.appengine.api import users
+from blog.forms import PostForm
 
 class MyTest(TestCase):
 
@@ -23,12 +24,18 @@ class MyTest(TestCase):
         return app
 
     def test_edit_url_resolves_to_edit_page_view(self):
+        posts, tags, categories = fetch_everything_from_db()
+        passed_days, remaining_days = calculate_work_date_stats()
+        form = PostForm()
 
+        response = self.client.get("/edit")
 
+        rendered_template = render_template('new_post.html',user_status=users.is_current_user_admin(),siteupdated="",\
+                           daysleft=remaining_days,dayspassed=passed_days,tags=tags,categories=categories,
+                           posts=posts.to_json(),
+                           codeversion=CODEVERSION)
 
-
-        response = self.client.get("/edit")#find the name of view
-      #  self.assertEqual(response, home_page)
+        self.assertEqual(rendered_template, response.data)
 
     def test_index_page_returns_correct_html(self):
         posts, tags, categories = fetch_everything_from_db()
