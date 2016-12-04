956b5b00e237aed934df5127a819f893e3aedb7f
Armen Arsakian <arsakian@ee.duth.gr>
2016-12-04 22:32:08 +0200

    new tag handling function

--- blog/utils.py
+++ blog/utils.py
@@ -2,3 +2,7 @@
 def find_tags_to_be_deleted_from_an_edited_post(editing_tags, existing_tags):
     tag_to_kept = set(editing_tags) & set(existing_tags)
     return set(existing_tags)-tag_to_kept
+
+
+def find_tags_to_added_from_an_edited_post(editing_tags, existing_tags):
+    return set(editing_tags) ^ set(editing_tags) & set(existing_tags)
