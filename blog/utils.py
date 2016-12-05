6a249160165076691ed67dbafe317df7ea64a4ca
Armen Arsakian <arsakian@ee.duth.gr>
2016-12-05 22:14:11 +0200

    edit post rewrite

--- blog/utils.py
+++ blog/utils.py
@@ -6,3 +6,7 @@ def find_tags_to_be_deleted_from_an_edited_post(editing_tags, existing_tags):
 
 def find_tags_to_added_from_an_edited_post(editing_tags, existing_tags):
     return set(editing_tags) ^ set(editing_tags) & set(existing_tags)
+
+
+def find_new_post_tags(old_post_tags, tags_to_be_deleted, tags_to_be_added):
+    return (set(old_post_tags) - tags_to_be_deleted) ^ tags_to_be_added
