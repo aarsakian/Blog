1b86fdcde10e8c44aeafa68e71f77da3f6f020ce
Armen Arsakian <arsakian@ee.duth.gr>
2017-02-15 19:47:26 +0200

    search/memcache api changes

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ import logging
 from google.appengine.ext import ndb
 from google.appengine.api import memcache, search
 
-from search import create_document
+from search import update_search_index
 
 
 POSTS_INDEX = "posts_idx"
@@ -15,6 +15,7 @@ class Tag(ndb.Model):
         tag_dict = self.to_dict()
         tag_dict["id"] = str(self.key.id())
         return tag_dict
+
 
 class Category(ndb.Model):
     category = ndb.StringProperty()
@@ -60,6 +61,9 @@ class BlogPost(ndb.Model):
     def get_tag_names(self):
         return [tag_key.get().tag for tag_key in self.tags if self.tags and tag_key.get()]
 
+    def get_category(self):
+        return self.category.get().category
+
     def _pre_put_hook(self):
         self.title = self.title.lstrip().rstrip()
 
@@ -75,9 +79,6 @@ class BlogList(list):
     def get_attr(cls):
         return str(cls.__name__.lower())
 
-    def update(self):
-        self._delete_memcache()
-        self._populate_memcache()
 
 
 
@@ -99,7 +100,6 @@ class Posts(BlogList, JsonMixin):
         #if not self._posts:
             self._posts = list(BlogPost.query().order(-BlogPost.timestamp))
             self._populate_memcache()
-            self._populate_search_index()
 
     @property
     def posts(self):
@@ -121,38 +121,17 @@ class Posts(BlogList, JsonMixin):
                     return True
         return False
 
-    def _populate_memcache(self):
-        """populate memcache
-        use key as post plus post.id"""
-        logging.info('cache is empty creating index')
-        if not memcache.add("POSTS_CACHE", self._posts):
-            logging.error("Memcache set failed for posts")
-            self._posts = list(BlogPost.query().order(-BlogPost.timestamp))
-
-    def _populate_search_index(self):
-        try:
-            for post in self._posts:
-                category = post.category.get().category
-                doc = create_document(post.key.id(), post.title, post.body,
-                                      category, post.timestamp)
-                search.Index(name=POSTS_INDEX).put(doc)
-
-        except search.Error:
-            logging.exception('Indexing failed')
-
-    def _delete_memcache(self):
-        logging.info('deleting cache for Posts')
-        if not memcache.delete('POSTS_CACHE') != 2:  # delete not successful
-            logging.error("Memcache delete failed for posts")
-
     def add(self, raw_title, raw_body, category_key, tags_ids, summary=None):
         post_key = BlogPost(title=raw_title,
                             body=raw_body,
                             category=category_key,
                             tags=tags_ids,
                             summary=summary).put()
-        self._posts.append(BlogPost.get_by_id(post_key.id()))
-        self.update()
+        post = post_key.get()
+        self._posts.append(post)
+        update_search_index(post.id(), post.title, post.body, post.summary,
+                        post.get_category(), post.timestamp)
+
         return post_key
 
     def get_tags(self):
@@ -165,15 +144,10 @@ class Posts(BlogList, JsonMixin):
         [other_tags.extend(post.get_tag_names()) for post in self._posts if post.key.id() != post_id]
         return other_tags
 
-    def update(self):
-        self._delete_memcache()
-        self._populate_memcache()
-
     def delete(self, post_key):
         post = post_key.get()
         [self._posts.pop(post_idx) for post_idx, post in enumerate(self._posts) if post.key == post_key]
         post.key.delete()
-        self.update()
 
     def get_by_title(self, title):
         for post in self._posts:
@@ -227,7 +201,7 @@ class Tags(BlogList, JsonMixin):
     def add(self, new_tags):
         new_tags_keys = [Tag(tag=new_tag).put() for new_tag in new_tags]
         self._tags.extend([tag_key.get() for tag_key in new_tags_keys])
-        self.update()
+
         return new_tags_keys
 
     def delete(self, tags_for_deletion):
@@ -236,7 +210,6 @@ class Tags(BlogList, JsonMixin):
                 tag.key.delete()
                 self._tags.pop(tag_idx)
                 self.delete(tags_for_deletion)
-        self.update()
 
     def get_keys(self, tags):
         return [tag.key for tag in self._tags if tag.tag in tags]
@@ -263,19 +236,10 @@ class Categories(BlogList):
     def __iter__(self):
         return (category.category for category in self._categories)
 
-    def _populate_memcache(self):
-        logging.info("populating cache for categories {}".format(self._categories))
-        if not memcache.add("CATEGORIES_CACHE", self._categories):
-            logging.error("Memcache set failed for categories")
-
-    def _delete_memcache(self):
-        if not memcache.delete('CATEGORIES_CACHE') != 2:
-            logging.error("Memcache delete failed for categories")
-
     def add(self, raw_category):
         category_key = Category(category=raw_category).put()
         self._categories.append(Category.get_by_id(category_key.id()))
-        self.update()
+
         return category_key
 
     def get_key(self, raw_category):
@@ -288,4 +252,4 @@ class Categories(BlogList):
                 category.key.delete()
                 self._categories.pop(cat_idx)
                 self.delete(category_for_deletion)
-        self.update()
+
--- blog/views.py
+++ blog/views.py
@@ -27,316 +27,11 @@ from utils import find_modified_tags, find_tags_to_be_removed, find_tags_to_be_a
 KEY="posts"
 TAG="tags"
 CATEGORY="categories"
-CODEVERSION=":v0.65"
+CODEVERSION=":v0.7"
 
 headerdict={"machine_learning":"Gaussian Graphical Models","programming":"Programming","about":"About Me"}
 months=['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
-# class Action(object):
-#     def __init__(self):
-#         """load basic members using memcache"""
-#         logging.info("initialized")
-#         self.posts_tags_db=[]
-#         self.catdict={}
-#         self.posts_tags_dict={}
-#         self.posts=memcache.get(KEY)
-#
-#
-#
-#         if isinstance(self.posts,list):self.nofposts=len(self.posts)-2
-#         else:self.nofposts=self.posts.count()-2
-#         self.tags=memcache.get(TAG)
-#         if self.tags is None:
-#             self.tags = Tag.all()
-#             memcache.add(TAG,self.tags)
-#         self.categories=memcache.get(CATEGORY)
-#         if self.categories is None:
-#             self.categories= Category.all()
-#             memcache.add(CATEGORY,self.categories)
-#
-#         for post in self.posts:
-#             logging.info(['posts',post.title])
-#             self.posts_tags_db.extend(post.tags)
-#             tags=[]
-#             for key in post.tags:tags.append(db.get(key).tag)
-#             self.posts_tags_dict[post.key]=tags
-#             self.catdict[post.category.key]=post.category.category
-#         logging.info(['catdict',self.catdict])
-#         self.tagnames=list(chain.from_iterable(self.posts_tags_dict.values()))
-#
-#
-#
-    
-    
-      
-    #
-    # @staticmethod
-    # def deleteMemcache(self):
-    #     memcache.delete(KEY)
-    #     memcache.delete(CATEGORY)
-    #     memcache.delete(TAG)
-    # @staticmethod
-    # def refetch(self):
-    #     self.posts =BlogPost.all().order('-timestamp')
-    #     memcache.add(KEY,self.posts)
-    #     self.tags = Tag.all()
-    #     memcache.add(TAG,self.tags)
-    #     self.categories= Category.all()
-    #     memcache.add(CATEGORY,self.categories)
-    #     createIndex(self.posts)
-    #
-    # def getall(self,catname=None,tagname=None):
-    #     data=[]
-    #
-    #     if catname:
-    #         catkey=[categoryobj.key for categoryobj in self.categories if categoryobj.category==catname][0]
-    #         posts=[]
-    #         [posts.append(post) for post in self.posts if catkey ==post.category.key]
-    #     #logging.info([catkey,len(categories)])
-    #     elif tagname:
-    #         logging.info(tagname)
-    #         tagkey=[tagobj.key for tagobj in self.tags if tagobj.tag==tagname]
-    #         if tagkey:tagkey=tagkey[0]
-    #         posts=[]
-    #         [posts.append(post) for post in self.posts if tagkey in post.tags]
-    #
-    #     else:posts=self.posts
-    #
-    #
-    #
-    #     for post in posts:
-    #
-    #         tags=[]
-    #         [tags.append({"tag":db.get(key).tag,"tagid":db.get(key).key.id()}) for key in post.tags]
-    #
-    #         updated=str(post.updated.day)+" "+str(months[post.updated.month])+" "+str(post.updated.year)
-    #         dateposted=str(post.timestamp.day)+" "+str(months[post.timestamp.month])+" "+str(post.timestamp.year)
-    #         data.append({"title":post.title,"body":post.body,"category":db.get(post.category.key).category,
-    #                      "catid": db.get(post.category.key).key.id(),"id":str(post.key.id()),\
-    #                     "tags":tags,"date":dateposted,"updated":updated})
-    #     return(data)
-
-
-
-# class APost(Action):
-#     def __init__(self,title=None,body=None,date=None,category=None,posttags=None,id=None):
-#
-#         Action.__init__(self)
-#         if id==None:
-#
-#             self.title=title
-#             self.body=body
-#             self.date=date
-#             self.postcategory=category
-#             self.posttags=posttags
-#
-#         elif title==None:
-#             self.obj =BlogPost.get_by_id(int(id))
-#             self.id = self.obj.key.id()
-#             self.post_tags_keys = self.obj.tags
-#
-#         else:
-#             self.obj = BlogPost.get_by_id(int(id))
-#             self.id = self.obj.key.id()
-#             self.post_tags_keys = self.obj.tags
-#             self.title=title
-#             self.body=body
-#             self.date=date
-#             self.postcategory=category
-#             self.posttags=posttags
-#
-#     def retrieve(self):
-#         """retrieves a post"""
-#         tags=[]
-#         [tags.append({"tag":db.get(key).tag,"tagid":db.get(key).key.id()}) for key in self.obj.tags]
-#
-#         data=[]
-#         updated=str(self.obj.updated.day)+" "+str(months[self.obj.updated.month])+" "+str(self.obj.updated.year)
-#         dateposted=str(self.obj.timestamp.day)+" "+str(months[self.obj.timestamp.month])+" "+str(self.obj.timestamp.year)
-#         data.append({"title":self.obj.title,"body":self.obj.body,"category":db.get(self.obj.category.key).category,
-#                          "catid": db.get(self.obj.category.key).key.id(),"id":str(self.obj.key.id()),\
-#              "tags":tags,"date":dateposted,"updated":updated})
-#         self.deleteMemcache(self)
-#         self.refetch(self)
-#         return(data)
-#
-#     def delete(self):
-#         """delete a post"""
-#         restTags=list(set(self.posts_tags_db)^set(self.post_tags_keys))
-#         logging.info(restTags)
-#         [db.get(tagkey).delete() for tagkey in    self.post_tags_keys  if tagkey not in restTags]
-#              #delete the tag if it does not exists in rest posts
-#         self.obj.delete()
-#         self.deleteMemcache(self)
-#         self.refetch(self)
-#         delete_document([str(self.obj.key)])
-#
-#     def update(self):
-#         """updates a post"""
-#
-#         #for catobj in self.categories:
-#         #    logging.info([catobj.key,catobj.category,catkeys])
-#         #    if catobj.category==self.postcatkey:#AN OLD CATEGORY
-#         #        catkey=catobj.key
-#         #    elif catobj.key not in self.catkeys:#not used category
-#         #        catobj.delete()
-#         #    else:
-#         #        logging.info(catobj.key.id())
-#                 #newcatobj=Category()
-#                 #newcatobj.category=category
-#                 #newcatobj.put()
-#                 #catkey=newcatobj.key
-#
-#
-#         post_tagsdb_values=[]
-#         post_tagsdb_keys=[]
-#         existingTags=[]
-#         existingTagskeys=[]
-#         tagsleft=[]
-#
-#        #find the existing tags of the post
-#         for tagkey in self.posts_tags_db:
-#             if tagkey not in self.post_tags_keys:
-#                 try:
-#                     tagsleft.append(Tag.get_by_id(tagkey.id()).tag)
-#                 except AttributeError:#ops a post without a tag
-#                     continue
-#             existingTagskeys.append(tagkey)
-#             existingTags.append(db.get(tagkey).tag) #existing Tags
-#
-#
-#
-#         for tagkey in self.post_tags_keys:post_tagsdb_values.append(db.get(tagkey).tag)#previous Tags of the post
-#
-#
-#      #   logging.info([self.posttags,type(self.posttags),type(post_tagsdb_values),post_tagsdb_values])
-#         unchangedtags=[]
-#         returnedTags=[]
-#       #  logging.info(['posttags',self.posttags,post_tagsdb_values])
-#         if post_tagsdb_values:#post does have tags
-#             logging.info(post_tagsdb_values)
-#             unchangedtags=set(self.posttags) & set( post_tagsdb_values)#changes tags added or removed
-#             newtags=set(self.posttags) ^ unchangedtags#new tags for this post
-#             oldtags=list(set(post_tagsdb_values)^unchangedtags)
-#             logging.info(["new",newtags,"old",oldtags,"unchanged",unchangedtags,list(unchangedtags)])
-#
-#             if list(unchangedtags):
-#                 unchangedtags=list(unchangedtags)
-#                 for tag in unchangedtags:
-#                     tagid=db.get(existingTagskeys[existingTags.index(tag)]).key.id()
-#                     returnedTags.append({"tag":tag,"tagid":tagid})
-#             else:unchangedtags=[]
-#             i=0
-#             logging.info(['Tags from other posts',existingTags])
-#             for tag in oldtags:#tags to be removed
-#
-#                 tag_key= existingTagskeys[existingTags.index(tag)]
-#                 if tag not in  tagsleft:#delete not used tags
-#                     tagobj=db.get(tag_key)
-#                     logging.info(["deleting",tag,tagobj])
-#                     tagobj.delete()
-#                 pos=post_tagsdb_values.index(tag)
-#
-#
-#                 self.obj.tags.pop(pos-i)
-#
-#                 i+=1
-#         elif  self.posttags:#new tags do exist
-#
-#             logging.info(self.posttags)
-#             newtags=set(self.posttags)#does not have tags
-#
-#         else:newtags=[]
-#
-#
-#
-#
-#         if newtags:
-#             for tag in list(newtags):#add new tag and update Post
-#                 logging.info(tag)
-#                 if tag not in existingTags:
-#                     tagobj=Tag()
-#                     tagobj.tag=tag
-#                     tagid=tagobj.put().id()
-#                     returnedTags.append({"tag":tag,"tagid":tagid})
-#
-#                 else:
-#                    tag_key= existingTagskeys[existingTags.index(tag)]
-#                    tagobj=Tag.get(tag_key)
-#                    returnedTags.append({"tag":tagobj.tag,"tagid":tagobj.key.id()})
-#                 self.obj.tags.append(tagobj.key)
-#         if isinstance(self.postcategory,list):self.postcategory=self.postcategory[0]
-#         logging.info([self.catdict.values()])
-#         self.obj.title=self.title
-#         self.obj.body=self.body
-#         self.obj.category=self.catdict.keys()[self.catdict.values().index(self.postcategory)]
-#         self.obj.updated=datetime.now()
-#         self.obj.put()
-#         createIndex([self.obj])
-#         tags=[]
-#         [tags.append({"tag":db.get(key).tag,"tagid":db.get(key).key.id()}) for key in self.obj.tags]
-#
-#         data=[]
-#         updated=str(self.obj.updated.day)+" "+str(months[self.obj.updated.month])+" "+str(self.obj.updated.year)
-#         dateposted=str(self.obj.timestamp.day)+" "+str(months[self.obj.timestamp.month])+" "+str(self.obj.timestamp.year)
-#         data.append({"title":self.obj.title,"body":self.obj.body,"category":db.get(self.obj.category.key).category,
-#                          "catid": db.get(self.obj.category.key).key.id(),"id":str(self.obj.key.id()),\
-#              "tags":tags,"date":dateposted,"updated":updated})
-#         self.deleteMemcache(self)
-#         self.refetch(self)
-#         return(data,returnedTags)
-#
-#
-#     def submitApost(self):
-#         returnedTags=[]
-#
-#         def Tagupdate (tag):
-#             #logging.info(self.tags.filter('tag',tag).count())
-#             if  tag!="" and self.tags.filter('tag',tag).count()==0:#tag does not exist
-#                 return(Tag(tag=tag).put())
-#             else:
-#                 return(self.tags.filter('tag',tag)[0].key)#otherwise find its key
-#
-#         posttagkeys=[]
-#
-#         if not self.tags:#Tags are empty therefore insert new tags
-#             posttagkeys=[Tag(tag=tag).put() for tag in self.posttags  if tag!=""]
-#         elif self.posttags[0]!="": posttagkeys=map(Tagupdate ,self.posttags)
-#         for key in posttagkeys:
-#             obj=db.get(key)
-#             returnedTags.append({"tag":obj.tag,"tagid":obj.key.id()})
-#         catnames=[]
-#         catkeys=[]
-#         if self.categories:   #categories exist make list of them
-#             [catnames.append(catobj.category) for catobj in self.categories]
-#             [catkeys.append(catobj.key) for catobj in self.categories]
-#             catobjs=dict(zip(catnames,catkeys))
-#             if  self.postcategory in catobjs.keys():catkey=catobjs[self.postcategory]
-#             else:#this post has a new category
-#                 newcatobj=Category()
-#                 newcatobj.category=self.postcategory
-#                 newcatobj.put()
-#                 catkey=newcatobj.key
-#         else:
-#             newcatobj=Category()
-#             newcatobj.category=self.postcategory
-#             newcatobj.put()
-#             catkey=newcatobj.key
-#
-#
-#
-#         post=BlogPost()
-#         post.title=self.title
-#         post.body=self.body
-#         post.tags=posttagkeys
-#         post.category=catkey
-#         post.put()
-#
-#         self.deleteMemcache(self)
-#         self.refetch(self)
-#         return(post.key,returnedTags)
-      
-   
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -464,7 +159,7 @@ def aboutpage(posts,tags,categories,action,siteupdated,daysleft,tz,dayspassed,da
     if request.args.get('q'):return redirect(url_for('searchresults',q=request.args.get('q')))    
     
     
-  
+
     return render_template('about.html',user_status=users.is_current_user_admin(),siteupdated=siteupdated,\
                            daysleft=daysleft,finaldate=tz,dayspassed=dayspassed.days,Post=aboutpost,codeversion=CODEVERSION)
 
@@ -949,25 +644,23 @@ def recent_feed():
 
 @app.route('/search',methods=['GET'])
 def searchsite():
-    data=[]
-    query_string=request.args.get('query', '')
+    data = []
+    query_string = request.args.get('query', '')
     try:
         query = search.Query(query_string=query_string, options=query_options)
 
         index = search.Index(name=_INDEX_NAME)
-
-        results=index.search(query)
-
+        results = index.search(query)
 
         if results:
             for scored_document in results:
-
+                print (scored_document.fields)
                 data.append({scored_document.fields[0].name:scored_document.fields[0].value,\
                              scored_document.fields[1].name:scored_document.fields[1].value,\
                              scored_document.fields[2].name:scored_document.fields[2].value,\
-                             "year":scored_document.fields[3].value.year,\
-                             "month":scored_document.fields[3].value.month})
-
+                             scored_document.fields[3].name:scored_document.fields[3].value,\
+                             "year":scored_document.fields[4].value.year,\
+                             "month":scored_document.fields[4].value.month})
 
         # process scored_document
     except search.Error:
