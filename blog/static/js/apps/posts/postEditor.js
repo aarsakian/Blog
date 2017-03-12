2d4c57c0239e7b33fdbb9743d8743547f31f34e9
Armen Arsakian <arsakian@ee.duth.gr>
2017-03-12 23:01:21 +0200

    editor layout

---
+++ blog/static/js/apps/posts/postEditor.js
@@ -0,0 +1,82 @@
+'use strict';
+
+class ContactFormLayout extends Layout {
+  constructor(options) {
+    super(options);
+    this.template = '#post-form-layout';
+    this.regions = {
+      preview: '#preview-container',
+      postform: '.post-form-container'
+    };
+  }
+
+  get className() {
+    
+  }
+}
+
+
+class PostPreview extends ModelView {
+  constructor(options) {
+    super(options);
+    this.template = '#post-form-preview';
+  }
+}
+
+
+class PostEditor {
+  constructor(options) {
+    this.region = options.region;
+
+    // Allow subapplication to listen and trigger events,
+    // useful for subapplication wide events
+    _.extend(this, Backbone.Events);
+  }
+
+  showEditor(contact) {
+    // Create the views
+    var layout = new ContactFormLayout({model: contact});
+
+    var postForm = new PostForm({model: contact});
+ //   var contactPreview = new PostPreview({model: contact});
+
+    // Render the views
+    this.region.show(layout);
+    layout.getRegion('postform').show(postForm);
+  //  layout.getRegion('preview').show(contactPreview);
+
+    this.listenTo(postForm, 'form:save', this.saveContact);
+    this.listenTo(postForm, 'form:cancel', this.cancel);
+  }
+
+  saveContact(contact) {
+    contact.save(null, {
+      success() {
+        // Redirect user to contact list after save
+        App.notifySuccess('Contact saved');
+        App.router.navigate('contacts', true);
+      },
+      error() {
+        // Show error message if something goes wrong
+        App.notifyError('Something goes wrong');
+      }
+    });
+  }
+
+  cancel() {
+    // Warn user before make redirection to prevent accidental
+    // cencel
+    App.askConfirmation('Changes will be lost', isConfirm => {
+      if (isConfirm) {
+        App.router.navigate('contacts', true);
+      }
+    });
+  }
+
+  // Close any active view and remove event listeners
+  // to prevent zombie functions
+  destroy() {
+    this.region.remove();
+    this.stopListening();
+  }
+}
