14ae4f23336ae2b81e360e84b78be018da578074
Armen Arsakian <arsakian@ee.duth.gr>
2017-09-15 23:20:54 +0300

    rewriting tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -4,7 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import unittest
+from selenium.common.exceptions import WebDriverException
+import unittest, time
+
+MAX_WAIT = 10
+
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 
 class NewVisitorTest(unittest.TestCase):  #
 
@@ -15,6 +32,7 @@ class NewVisitorTest(unittest.TestCase):  #
 
     def tearDown(self):  #
         self.browser.quit()
+
 
     def can_login(self):
 
@@ -27,13 +45,27 @@ class NewVisitorTest(unittest.TestCase):  #
         self.browser.find_element_by_id("submit-login").click()
 
         # after I login i should see an edit link
-        try:
-            edit_element = WebDriverWait(self.browser, 5).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "edit")))
-        except:
-            self.fail("did not locate the element")
-        finally:
-            self.assertEqual("Edit", edit_element.text)
+        self.wait_to_be_logged_in()
+
+    @wait
+    def wait_to_be_logged_in(self):
+
+        edit_element = self.browser.find_element_by_class_name("edit")
+        self.assertEqual("Edit", edit_element.text)
+
+    @wait
+    def wait_to_find_a_post(self):
+        title_element = self.browser.find_element_by_css_selector(".postTitle h3 a").text
+        body_element = self.browser.find_element_by_css_selector(".article").text
+
+
+        tags = self.browser.find_elements_by_class_name("tag")
+        post_key = self.browser.find_element_by_css_selector("[data-id]").text
+       # print (title_element, body_element, tags)
+        self.assertEqual(u"introducing TDD requires discipline which is not given", body_element)
+        [self.assertIn(tag.text, "tag1, tag2") for tag in tags]
+        self.assertEqual(u"my ultimate blog post", title_element)
+        self.assertNotEqual(post_key, "")
 
     def test_can_create_a_post_and_retrieve_it_later(self):  #
         #  after login I am directed to the main page
@@ -54,8 +86,8 @@ class NewVisitorTest(unittest.TestCase):  #
         summary_field = self.browser.find_element_by_id("new-post-summary")
 
         new_post_title_field.send_keys('my ultimate blog post')
-        new_post_body_textfield.clear()
 
+        new_post_body_textfield.clear()
         new_post_body_textfield.send_keys('introducing TDD requires discipline which is not given')
 
         new_post_category_field.send_keys('cat1')
@@ -65,24 +97,9 @@ class NewVisitorTest(unittest.TestCase):  #
         summary_field.send_keys("a little summary")
         # There is a submit button to post the article
         self.browser.find_element_by_id("submit").click()
-        post_key = ""
 
-        try:
-            wait = WebDriverWait(self.browser, 60)
+        self.wait_to_find_a_post()
 
-            title_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".postTitle h3 a"))).text
-            body_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".article"))).text
-
-            tags = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "tag")))
-            post_key = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data_element="data-id"]'))).text
-
-
-        finally:
-
-            self.assertEqual(u"my ultimate blog post", title_element)
-            self.assertEqual(u"introducing TDD requires discipline which is not given", body_element)
-            [self.assertIn(tag.text, "tag1, tag2") for tag in tags]
-            self.assertNotEqual(post_key, "")
 
 
     # def test_can_view_all_posts_in_landing_page(self):
