72e32e921966d9707fa82ea04b0064d8a6b85368
Armen Arsakian <arsakian@digitalforensics.gr>
2017-11-08 17:16:00 +0200

    t

--- blog/settings.py
+++
@@ -1,6 +0,0 @@
-DEBUG=False
-SECRET_KEY='dev_42asdw24w3fddfsa45i334tmfsd@$harfgsd324m'
-CSRF_ENABLED=True
-CSRF_SESSION_KEY='d548ijmwfe#@$r+'
-#user:password@host/dbname
-SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms://armen:s0p@lious@google.com/test'
