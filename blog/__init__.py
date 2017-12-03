8f3d3572a4fae4eec2d35775c9a9d89d275f7e75
Armen Arsakian <arsakian@ee.duth.gr>
2017-12-03 20:02:10 +0200

    removed csrf code not essential

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,14 +6,11 @@ from flask_wtf.csrf import  CsrfProtect
 bootstrap = Bootstrap()
 
 
-csrf = CsrfProtect()
-
 
 def create_app():
    app = Flask(__name__)
    app.config.from_object(Config)
    bootstrap.init_app(app)
-   csrf.init_app(app)
 
    return app
 
