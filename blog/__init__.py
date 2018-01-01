0332d96f79a1cf2b1b7313848b504126ff4527cd
Armen Arsakian <arsakian@ee.duth.gr>
2018-01-01 13:13:40 +0200

    preparing for continuous deployment

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,15 @@
+# appengine_config.py
+from google.appengine.ext import vendor
+
+# Add any libraries install in the "lib" folder.
+vendor.add('lib')
+
 from flask import Flask
 from settings import Config
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import  CsrfProtect
+
+
 
 bootstrap = Bootstrap()
 
