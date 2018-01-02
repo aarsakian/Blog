f1abcfb8130c96823776140ddc68e09350fd3760
Armen Arsakian <arsakian@ee.duth.gr>
2018-01-02 20:51:45 +0200

    moved secret_key funcionality to settings

--- blog/settings.py
+++ blog/settings.py
@@ -1,5 +1,24 @@
-import os
+from google.appengine.ext import ndb
 
+class Settings(ndb.Model):
+  name = ndb.StringProperty()
+  value = ndb.StringProperty()
+
+  @staticmethod
+  def get(name):
+    NOT_SET_VALUE = "NOT SET"
+    retval = Settings.query(Settings.name == name).get()
+    if not retval:
+      retval = Settings()
+      retval.name = name
+      retval.value = NOT_SET_VALUE
+      retval.put()
+    if retval.value == NOT_SET_VALUE:
+      raise Exception(('Setting %s not found in the database. A placeholder ' +
+        'record has been created. Go to the Developers Console for your app ' +
+        'in App Engine, look up the Settings record with name=%s and enter ' +
+        'its value in that record\'s value field.') % (name, name))
+    return retval.value
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = Settings.get('FLASK_SECRET_KEY')
