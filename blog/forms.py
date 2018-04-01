7a64242aa1828f195a24dc0707a857ea037b5b5c
Armen Arsakian <arsakian@digitalforensics.gr>
2018-04-01 11:40:52 +0300

    added radio buttons

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, RadioField, FieldList, FormField
 from wtforms.validators import DataRequired
+
+import logging
+
+class AnswersField(RadioField):
+    def _value(self):
+        if self.data:
+            logging.info("VAAL {}".format(self.data))
+            return u', '.join(self.data)
+        else:
+
+            return u''
+
+
+class IMForm(FlaskForm):
+    protocol = RadioField(choices=[('aim', 'AIM'), ('msn', 'MSN')], default="{{radio_field}}")
+
+
 
 
 class PostForm(FlaskForm):
@@ -9,4 +26,6 @@ class PostForm(FlaskForm):
     summary = StringField("", default="{{summary}}")
     tags = StringField("", default="{{tags}}")
     category = StringField("", validators=[DataRequired()], default="{{category}}")
+    radio_field = FieldList(FormField(IMForm))
     submit = SubmitField('Save')
+
