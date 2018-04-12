8ba6e5d5bb0990322d640e99e2db9588c170d942
Armen Arsakian <arsakian@ee.duth.gr>
2018-04-12 14:00:16 +0300

    added csrf

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 from flask_bootstrap import Bootstrap
 from flask_sitemap import Sitemap
+from flask_wtf.csrf import CSRFProtect
+
 
 import assets
 
@@ -12,14 +14,15 @@ bootstrap = Bootstrap()
 
 sitemap = Sitemap()
 
+csrf = CSRFProtect()
+
 app = Flask(__name__, root_path='blog',  instance_relative_config=True)
 
 bootstrap.init_app(app)
 
 sitemap.init_app(app)
 
-
-
+csrf.init_app(app)
 
 
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
