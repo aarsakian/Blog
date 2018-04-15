8c972662c20a810ff7c75221d0f982efbf6d00a3
Armen Arsakian <arsakian@ee.duth.gr>
2018-04-15 22:43:14 +0300

    fixed wrong naming

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired
 import logging
 
 class AnswerField(FlaskForm):
-    p_answer = StringField("", default="{{answer}}")
-    is_correct = BooleanField(default="{{is_correct}}")
+    p_answer = StringField("", default="{{p_answer}}", _name="")
+    is_correct = BooleanField(default="{{is_correct}}", _name="")
 
 
 
@@ -16,6 +16,6 @@ class PostForm(FlaskForm):
     summary = StringField("", default="{{summary}}")
     tags = StringField("", default="{{tags}}")
     category = StringField("", validators=[DataRequired()], default="{{category}}")
-    answers = FieldList(FormField(AnswerField), min_entries=4)
+    answers = FieldList(FormField(AnswerField), min_entries=1)
     submit = SubmitField('Save')
 
