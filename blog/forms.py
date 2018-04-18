9be461419b40706d5a0e03b1e0cdad33c5cc0ee3
Armen Arsakian <arsakian@ee.duth.gr>
2018-04-18 22:07:46 +0300

    new radio field for answers

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField, FieldList, FormField, Field
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, FieldList, FormField, Field, RadioField
 from wtforms.validators import DataRequired
 
 import logging
 
 class AnswerField(FlaskForm):
-    p_answer = StringField("", default="{{p_answer}}", _name="")
+    p_answer = StringField("", default="{{this.p_answer}}", _name="")
     is_correct = BooleanField(default="{{is_correct}}", _name="")
 
 
+
+class AnswerRadioField(FlaskForm):
+    r_answers = RadioField("")
 
 class PostForm(FlaskForm):
     title = StringField("", validators=[DataRequired()], default="{{title}}")
@@ -16,6 +19,6 @@ class PostForm(FlaskForm):
     summary = StringField("", default="{{summary}}")
     tags = StringField("", default="{{tags}}")
     category = StringField("", validators=[DataRequired()], default="{{category}}")
-    answers = FieldList(FormField(AnswerField), min_entries=1)
+    answers = FieldList(FormField(AnswerField), min_entries=4)
     submit = SubmitField('Save')
 
