ddbc18bca5b2feb258d05223c9dcc349dce27e45
Armen Arsakian <arsakian@ee.duth.gr>
2018-05-15 20:21:40 +0300

    added support for minified html

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,7 +5,6 @@ from flask_sitemap import Sitemap
 from flask_wtf.csrf import CSRFProtect
 
 
-import assets
 
 import os
 
@@ -16,22 +15,33 @@ sitemap = Sitemap()
 
 csrf = CSRFProtect()
 
-app = Flask(__name__, root_path='blog',  instance_relative_config=True)
+
+
+
+
+
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+    app = Flask(__name__, root_path='blog',
+                template_folder= 'templates/production',
+                instance_relative_config = True)
+
+
+    app.config.from_object('blog.settings.Production')
+    app.jinja_env.globals['DEV'] = False
+else:
+
+    app = Flask(__name__, root_path='blog',
+                instance_relative_config = True)
+
+    app.config.from_object('blog.settings.Testing')
+    app.jinja_env.globals['DEV'] = True
+
 
 bootstrap.init_app(app)
 
 sitemap.init_app(app)
 
 csrf.init_app(app)
-
-
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    app.config.from_object('blog.settings.Production')
-    app.jinja_env.globals['DEV'] = False
-else:
-    app.config.from_object('blog.settings.Testing')
-    app.jinja_env.globals['DEV'] = True
-    bundles = assets.init(app)
 
 
 import views
