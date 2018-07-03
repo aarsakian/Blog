1190ac9985f8b3fb007af9e2d9db2acc65291f90
Armen Arsakian <arsakian@ee.duth.gr>
2018-07-03 22:06:27 +0300

    removed flask_pagedown dependency

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sitemap import Sitemap
 from flask_wtf.csrf import CSRFProtect
-from flask_pagedown import PageDown
+
 
 
 
@@ -17,7 +17,6 @@ sitemap = Sitemap()
 csrf = CSRFProtect()
 
 
-pagedown = PageDown()
 
 
 
