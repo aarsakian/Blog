10c5e13e0d025ad6fc48ce3f5fdd047f5525267e
Armen Arsakian <arsakian@ee.duth.gr>
2018-09-11 12:17:02 +0300

    only view based functionality for notifications

--- blog/static/js/apps/notifications/app.js
+++ blog/static/js/apps/notifications/app.js
@@ -8,74 +8,38 @@ var ModelView = require('../../common').ModelView;
 var Notification = require('./models/notification')
 
 
-class NotificationLayout extends Layout {
-  constructor(options) {
-    super({tagName:"div", className:"row col-12"});
-
-    this.template = '#notification-layout';
-    this.regions = {
-      messages: '.notification-messages'
-
-
-    };
-  }
-
-}
 
 
 class NotificationView extends ModelView {
   constructor(options) {
-    super(options);
+    super({tagName:"div", className:"row col-12"});
+
     this.template = '#notifications-template';
 
-    this.model = new Notification({});
+  }
+
+  showMessage(msg, type) {
+      this.model = new Notification({"message":msg,
+                                    "type":type});
+
+      var view  = this.render();
+      this.fadeMessage();
 
   }
 
+  fadeMessage() {
 
-  initialize(options) {
-    this.listenTo(this.model, 'change', this.render);
-
-  //  var tagList = new TagListView({collection: options.model.tags});
+    var view = this;
+    this.$(".alert").fadeTo(2000, 500).slideUp(500, function(){
+        view.$(".alert").slideUp(500);
+    });
 
   }
 
-  showMessage(msg) {
-      console.log("NOTF"+this.model);
-      this.model.set("message", msg);
-  }
 
 }
 
 
-class Notifier {
-  constructor(options) {
-
-    // Region where the application will be placed
-     this.region = options.region;
-
-    // Allow subapplication to listen and trigger events,
-    // useful for subapplication wide events
-    _.extend(this, Backbone.Events);
-  }
-
-
-  showSuccessMessage(msg) {
-
-      layout = new NotificationLayout();
-      this.region.show(layout);
-
-      var notificationView = new NotificationView({model: new Notification({message:msg})});
-
-      layout.getRegion('messages').show(notificationView);
-
-  }
-
-  destroy() {
-    this.region.remove();
-    this.stopListening();
-  }
-}
 
 
 module.exports = NotificationView;
