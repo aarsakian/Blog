9e7bc09641df8b2731896e358fcd0d8daa2d72ee
Armen Arsakian <arsakian@ee.duth.gr>
2018-11-13 22:37:44 +0200

    init for service workers

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,3 +1,18 @@
 var App = require('./app');
 
+function initServiceWorker() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/static/js/sw.js').then(registration => {
+            console.log('SW registered: ', registration);
+          }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+          });
+        });
+      }
+
+}
+
+initServiceWorker();
+
 App.start();
