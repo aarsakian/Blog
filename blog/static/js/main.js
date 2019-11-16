7a8459614e06566c48459d53b8988b8fdd3d34c5
Armen Arsakian <arsakian@ee.duth.gr>
2019-11-16 20:38:52 +0200

    removed log message

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -4,7 +4,6 @@ function initServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
           navigator.serviceWorker.register('/static/js/sw.js').then(registration => {
-            console.log('SW registered: ', registration);
           }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
           });
