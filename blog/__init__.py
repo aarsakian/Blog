1c7f9613118656171d5979036150c43cbc546a96
Armen Arsakian <aarsakian@gmail.com>
2020-01-02 22:35:41 +0200

    testing emulator env

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,8 +4,10 @@ from flask_bootstrap import Bootstrap
 from flask_sitemap import Sitemap
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
-from google.cloud import ndb
-from google.oauth2 import service_account
+from google.cloud import ndb, storage
+import google.auth.credentials
+import mock
+
 
 
 import os
@@ -19,7 +21,35 @@ csrf = CSRFProtect()
 
 login_manager = LoginManager()
 
-client = ndb.Client()
+
+
+
+
+
+
+
+if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+
+    app = Flask(__name__, root_path='blog',
+                template_folder= 'templates/production',
+                instance_relative_config = True)
+
+    app.config.from_object('blog.settings.Production')
+    app.jinja_env.globals['DEV'] = False
+    client = ndb.Client()
+    storage_client = storage.Client()
+else:
+    app = Flask(__name__)
+
+    os.environ["DATASTORE_EMULATOR_HOST"] = "localhost:8081"
+    os.environ["DATASTORE_PROJECT_ID"] = "test"
+
+    credentials = mock.Mock(spec=google.auth.credentials.Credentials)
+    client = ndb.Client(project="test", credentials=credentials)
+    storage_client = storage.Client(project="test", credentials=credentials)
+
+    app.config.from_object('blog.settings.Testing')
+    app.jinja_env.globals['DEV'] = True
 
 
 def ndb_wsgi_middleware(wsgi_app):
@@ -28,25 +58,6 @@ def ndb_wsgi_middleware(wsgi_app):
             return wsgi_app(environ, start_response)
 
     return middleware
-
-
-
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    print ("PRODUCTION")
-    app = Flask(__name__, root_path='blog',
-                template_folder= 'templates/production',
-                instance_relative_config = True)
-
-
-    app.config.from_object('blog.settings.Production')
-    app.jinja_env.globals['DEV'] = False
-else:
-    print ("DEVEs")
-    app = Flask(__name__)
-
-    app.config.from_object('blog.settings.Testing')
-    app.jinja_env.globals['DEV'] = True
-
 
 
 bootstrap.init_app(app)
@@ -58,6 +69,7 @@ csrf.init_app(app)
 login_manager.init_app(app)
 # Wrap the app in middleware.
 app.wsgi_app = ndb_wsgi_middleware(app.wsgi_app)
+
 
 from . import views
 
