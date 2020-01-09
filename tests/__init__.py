7964c186e8021ade77b1c3e6aba6853db632afb8
Armen Arsakian <aarsakian@gmail.com>
2020-01-09 21:18:36 +0200

    migrated util tests to pytest

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,31 @@ from blog.models import Tags, Posts, Categories, BlogPost, Answer
 from flask_testing import TestCase
 
 
+def assertEqualHTML(string1, string2, file1='', file2=''):
+    """
+    Compare two unicode strings containing HTML.
+    A human friendly diff goes to logging.error() if there
+    are not equal, and an exception gets raised.
+    """
+
+    def short(mystr):
+        max = 20
+        if len(mystr) > max:
+            return mystr[:max]
+        return mystr
+
+    p = []
+    for mystr, file in [(string1, file1), (string2, file2)]:
+        if not isinstance(mystr, unicode):
+            raise Exception(u'string ist not unicode: %r %s' % (short(mystr), file))
+        soup = bs(mystr, 'html.parser')
+        pretty = soup.prettify()
+        p.append(pretty)
+    if p[0] != p[1]:
+        for line in difflib.unified_diff(p[0].splitlines(), p[1].splitlines(), fromfile=file1, tofile=file2):
+            logging.error(line)
+        raise Exception('Not equal %s %s' % (file1, file2))
+
 
 class BlogTestBase(TestCase):
     maxDiff = None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,53 +1,57 @@
-
+import pytest
 from blog.utils import find_modified_tags, datetimeformat, make_external,  calculate_work_date_stats, to_markdown, \
     allowed_file
-from google.appengine.ext import testbed
 
-
-
+from tests import assertEqualHTML
 
 DATEFORMAT = '%A, %d %B %Y'
 
 
-from . import BlogTestBase
+def test_to_markdown():
+    markdown_text = "body of post"
+    u"<p>{}</p>\n".format(markdown_text) == to_markdown(markdown_text)
 
-class TestUtils(BlogTestBase):
-    maxDiff = None
 
-    def setUp(self):
-        self.testbed = testbed.Testbed()
-        # Then activate the testbed, which prepares the service stubs for use.
-        self.testbed.activate()
+def test_for_xss_input():
+    xss_input = 'an <script>evil()</script> example'
 
-    def test_to_markdown(self):
-        markdown_text = "body of post"
-        self.assertEqual(u"<p>{}</p>\n".format(markdown_text), to_markdown(markdown_text))
+    assert "{}".format(xss_input) != to_markdown(xss_input)
+    assert u"<p>an &lt;script&gt;evil()&lt;/script&gt; example</p>\n" ==\
+                     to_markdown(xss_input)
 
-    def test_for_xss_input(self):
-        xss_input = 'an <script>evil()</script> example'
 
-        self.assertNotEqual(u"{}".format(xss_input), to_markdown(xss_input))
-        self.assertEqual(u"<p>an &lt;script&gt;evil()&lt;/script&gt; example</p>\n",
-                         to_markdown(xss_input))
+def test_text_with_links_renders_to_html():
+    text_with_link = "[I'm an inline-style link](https://www.google.com)"
+    '<p><a href="https://www.google.com" rel="nofollow">' ==\
+                     'I\'m an inline-style link</a></p>\n', to_markdown(text_with_link)
 
-    def test_text_with_links_renders_to_html(self):
-        text_with_link = "[I'm an inline-style link](https://www.google.com)"
-        self.assertEqual('<p><a href="https://www.google.com" rel="nofollow">'
-                         'I\'m an inline-style link</a></p>\n', to_markdown(text_with_link))
 
-    def test_table_renders_to_html_with_classed(self):
-        table = "| Header 1 | Header 2 |\n" \
-                "| -------- | -------- |\n" \
-                "| Cell 1 | Cell 2 |"
+def test_table_renders_to_html_with_classed():
+    table = "| Header 1 | Header 2 |\n" \
+            "| -------- | -------- |\n" \
+            "| Cell 1 | Cell 2 |"
 
-        rendered_table = u"<table class='table table-bordered'><thead><tr><th>Header 1</th><th>Header 2</th></tr>" \
-                         u"</thead>" \
-                         u"<tbody><tr><td>Cell 1</td><td>Cell 2</td></tr></tbody></table>"
+    rendered_table = u"<table class='table table-bordered'><thead><tr><th>Header 1</th><th>Header 2</th></tr>" \
+                     u"</thead>" \
+                     u"<tbody><tr><td>Cell 1</td><td>Cell 2</td></tr></tbody></table>"
+    with pytest.raises(Exception):
+        assertEqualHTML(rendered_table, to_markdown(table).strip())
 
-        self.assertEqualHTML(rendered_table, to_markdown(table).strip())
 
-    def test_allowed_extensions(self):
-        not_allowed_file = 'test.txt'
-        allowed_file_ext = 'myphoto.jpg'
-        self.assertFalse(allowed_file(not_allowed_file))
-        self.assertTrue(allowed_file(allowed_file_ext))
+def test_allowed_extensions():
+    not_allowed_file = 'test.txt'
+    allowed_file_ext = 'myphoto.jpg'
+    assert not allowed_file(not_allowed_file)
+    assert allowed_file(allowed_file_ext)
+
+
+def test_find_modified_tags():
+    test_existing_tags = ["a new tag", "a new new tag"]
+    editing_tags1 = ["a new tag 1", "a new new tag 2"]
+    editing_tags2 = ["a new tag 1", "a new tag"]
+
+    modified_tags = find_modified_tags(editing_tags1, test_existing_tags)
+    assert modified_tags == editing_tags1
+
+    modified_tags = find_modified_tags(editing_tags2, test_existing_tags)
+    assert modified_tags == ["a new tag 1"]
